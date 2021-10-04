578a0553400e9ad999d8a7666eae2fb97575d384
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-10-04 10:24:55 +0530

    [akhila]Refcator:form validation in registration page

--- Js/registration.js
+++ Js/registration.js
@@ -1,135 +1,69 @@
+       
+    // firstname validation
 
-window.addEventListener('DOMContentLoaded',(event) => {         
-    // name validation
-    const name = document.getElementById("first_name");       
-    const nameError = document.getElementById("firstname_error");
-    const form = document.getElementById("form");
-    let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{3,}$');
-    form.addEventListener("submit", (e) => {
-        let messages = []
-        if(name.value === '' || name.value == null){
-            messages.push('firstname is required');
-        }
-        if(messages.length > 0){
-            e.preventDefault()
-            nameError.innerText = messages.join(',');
-        }
-    });
-        name.addEventListener("input", function () {
-        if (nameRegex.test(name.value)) {
-            nameError.textContent = "";    
-
-        }
-        if (!nameRegex.test(name.value)) {
-            nameError.textContent = "enter firstname";                 
-        }
-    });          
-    var lastname = document.querySelector("#last_name");
-    var lastnameError = document.querySelector("#lastname_error");
-    const form1 = document.getElementById("form");
-    let lastnameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{3,}$');
-    form1.addEventListener("submit", (e) => {
-        let messages = []
-        if(lastname.value === '' || lastname.value == null){
-            messages.push('lastname is required');
-        }
-        if(messages.length > 0){
-            e.preventDefault()
-            lastnameError.innerText = messages.join(',');
-        }
-    });
-    lastname.addEventListener("input", function () {
-
-        if (lastnameRegex.test(lastname.value)) {
-            lastnameError.textContent = "";
-
-        }
-        if (!lastnameRegex.test(lastname.value)) {
-            lastnameError.textContent = "invalid lastname";
-
-        }
-    });      
-
-    // email validation
-    var email = document.querySelector("#email");
-    var emailError = document.querySelector("#email_error");
-    const form2 = document.getElementById("form");
-    form2.addEventListener("submit", (e) => {
-        let messages = []
-        if(email.value === '' || email.value == null){
-            messages.push('email is required');
-        }
-        if(messages.length > 0){
-            e.preventDefault()
-            emailError.innerText = messages.join(',');
-        }
-    });
-    email.addEventListener("input", function () {
-        let emailRegex = RegExp("^[A-Za-z0-9]+[.+-]{0,1}[0-9a-zA-Z]$");
-        if (emailRegex.test(email.value)) {
-            emailError.textContent = "";
-
-        } else {
-            emailError.textContent = "InvalidEmail";
-
-        }
-    });       
+    function validatefirstName() {
+        const name = document.getElementById('first_name');       
+        const nameError = document.getElementById('firstname_error');
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{3,}$');  
     
+        if(nameRegex.test(name.value)) 
+              nameError.textContent = "";
+        else nameError.textContent = "Enter your first name";        
+    }
+    //lastname validation
 
-    // password validation
-    var pwd = document.querySelector("#password");
-    var pwdError = document.querySelector("#password_error");
-    const form3 = document.getElementById("form");
-    form3.addEventListener("submit", (e) => {
-        let messages = []
-        if(pwd.value === '' || pwd.value == null){
-            messages.push('password is required');
-        }
-        if(messages.length > 0){
-            e.preventDefault()
-            pwdError.innerText = messages.join(',');
-        }
-    });
-    pwd.addEventListener("input", function () {
-        let passwordRegex = RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,}$");
-        if (passwordRegex.test(pwd.value)) {
-            pwdError.textContent = "";
-        }
-        if (pwd.value.length < 8) {
-            pwdError.textContent = "Password Invalid";
-        }
-        if (!passwordRegex.test(pwd.value)) {
-            pwdError.textContent = "Password Invalid";
-        }
-    });
-    var confirmpwd = document.querySelector("#confirm_password");
-    var confirmpwdError = document.querySelector("#confirmpwd_error");
-    const form4 = document.getElementById("form");
-    form4.addEventListener("submit", (e) => {
-        let messages = []
-        if(confirmpwd.value === '' || confirmpwd.value == null){
-            messages.push('password is required');
-        }
-        if(messages.length > 0){
-            e.preventDefault()
-            confirmpwdError.innerText = messages.join(',');
-        }
-    });
-    confirmpwd.addEventListener("input", function () {
-        let confirmpasswordRegex = RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,}$");
-        if (confirmpasswordRegex.test(confirmpwd.value)) {
-            confirmpwdError.textContent = "";
-        }
-        if (confirmpwd.value.length < 8) {
-            confirmpwdError.textContent = "Password Invalid";
-        }
-        if (!confirmpasswordRegex.test(confirmpwd.value)) {
-            confirmpwdError.textContent = "Password Invalid";
-        }
-    });
-});
+    function validatelastName() {
+        var lastname = document.getElementById('last_name'); 
+        var lastnameError = document.getElementById('lastname_error');
+        let lastnameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{3,}$');  
+    
+        if(lastnameRegex.test(lastname.value)) 
+              lastnameError.textContent = "";
+        else lastnameError.textContent = "Enter your last name";        
+    }
 
-     function next () {
+  // phone number validation
+
+function validateconfirmpwd() {
+    const phone = document.getElementById('confirm_password');
+    const phnError = document.getElementById('confirmpwd_error');
+    let numRegex = RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,}$");
+    if (numRegex.test(phone.value))
+        phnError.textContent = "";
+    else phnError.textContent = "Enter your confirm password";
+}
+
+//password validation
+
+    function validatePwd() {
+        var pwd = document.getElementById('password');
+        var pwdError = document.getElementById('password_error');
+       let passwordRegex = RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()]).{8,}$");
+        if(passwordRegex.test(pwd.value)) 
+              pwdError.textContent = "";
+        else 
+        pwdError.textContent = "Enter your password";        
+    }
+
+
+    // Email validation
+
+    function validateEmail() {
+        var email = document.getElementById('email');
+        var emailError = document.getElementById('email_error');
+        let emailRegex = RegExp("^[A-Za-z0-9]+[.+-]{0,1}[0-9a-zA-Z]$");
+    
+        if(emailRegex.test(email.value)) 
+              emailError.textContent = "";
+        else emailError.textContent = "Enter your email";        
+    }
+    
+function next () {
+    validatefirstName();
+    validatelastName();
+    validatePwd();
+    validateconfirmpwd();
+    validateEmail();
        
          let data={
              "firstName" : document.getElementById("first_name").value,
